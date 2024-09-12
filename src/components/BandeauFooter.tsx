c638c308b5dce1d4fc91a9e47ce57d13aaeeb870
François Giorgi <77675039+tchoifr@users.noreply.github.com>
2024-09-12 15:53:10 +0200

    logique btn retour

--- src/components/BandeauFooter.tsx
+++ src/components/BandeauFooter.tsx
@@ -1,7 +1,12 @@
 import React from "react";
 import dbm from "../assets/images/DBM.png";
 import btnRetour from "../assets/images/btnRetour.png";
+
 const BandeauFooter = () => {
+  const scrollToTop = () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  };
+
   return (
     <div className="flex h-16 items-center justify-around mt-10 border bg-white">
       <div className="flex pl-60">
@@ -9,8 +14,13 @@ const BandeauFooter = () => {
         <p className="p-5">Mentions légales</p>
         <p className="p-5">Politique de confidentialité des données</p>
       </div>
-      <img src={dbm} alt="" />
-      <img src={btnRetour} alt="" />
+      <img src={dbm} alt="DBM Logo" />
+      <img
+        src={btnRetour}
+        alt="Retour en haut"
+        className="cursor-pointer"
+        onClick={scrollToTop}
+      />
     </div>
   );
 };
